8e1a741f8ee72ab418e05a687fc048fe377f1397
Derrick <derrickzhuyz@outlook.com>
2021-01-28 22:17:08 +0800

    zhuyizhang 修改1
    level 1

--- pages/index0/index0.js
+++ pages/index0/index0.js
@@ -1,66 +1,23 @@
 // pages/index0/index0.js
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    passageIconSrc: '/icons/passage-icon.jpg',
+    musicIconSrc: '/icons/music-icon.jpg',
+    comicIconSrc: '/icons/comic-icon.jpg',
+    radioIconSrc: '/icons/radio-icon.jpg',
+    contactIconSrc: '/icons/contact-icon.jpg'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+ 
+  onLoad: function() {
+    var that = this
+    var picList = []
+    picList.push("http://psyhelp.cqu.edu.cn/__local/7/3C/C9/9CAD953BE2E601CB4C43B8DED09_5E067D77_7862.jpg")
+    picList.push("http://psyhelp.cqu.edu.cn/__local/4/B3/F0/99C465764EF6C44E1E135455D6D_C1C5E3D5_662F.jpg")
+    picList.push("http://psyhelp.cqu.edu.cn/__local/1/92/9E/5C150AA892CD571F2850AF99F26_28411FCE_9F4D.jpg")
+    that.setData({
+      picList: picList,
+    })
   }
 })
